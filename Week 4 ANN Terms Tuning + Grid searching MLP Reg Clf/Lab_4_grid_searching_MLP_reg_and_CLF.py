dc1cb73f4051f0331a943dfdfe721444c41a4f93
CC <89727825+coDingcaRot@users.noreply.github.com>
2025-01-30 12:10:27 -0800

    Revised Lab 4 Exercise 6

--- Week 4 ANN Terms Tuning + Grid searching MLP Reg Clf/Lab_4_grid_searching_MLP_reg_and_CLF.py
+++ Week 4 ANN Terms Tuning + Grid searching MLP Reg Clf/Lab_4_grid_searching_MLP_reg_and_CLF.py
@@ -448,6 +448,7 @@ def ex4_Gridsearching_MLPClf():
 # ex4_Gridsearching_MLPClf()
 
 # example 5 MLP regressors
+# RMSE: 24.974816119622876
 def ex5_MLP_regressors():
     import pandas as pd
     import numpy as np
@@ -461,8 +462,8 @@ def ex5_MLP_regressors():
     warnings.filterwarnings(action='once')
 
     PATH = "../Datasets/"
-    CSV_DATA = "USA_Housing.csv"
-    df = pd.read_csv(PATH + CSV_DATA)
+    CSV_DATA = "housing.data"
+    df = pd.read_csv(PATH + CSV_DATA, header=None)
 
     # Show all columns.
     pd.set_option('display.max_columns', None)
@@ -477,10 +478,8 @@ def ex5_MLP_regressors():
     dataset = df.values
 
     # split into input (X) and output (Y) variables
-    X = df.copy()
-    y = X['Price']
-    del X['Price']
-    del X['Address']
+    X = dataset[:, 0:13]
+    y = dataset[:, 13]
 
     trainX, temp_X, trainY, temp_y = train_test_split(X, y, train_size=0.7)
     valX, testX, valY, testY = train_test_split(temp_X, temp_y, train_size=0.5)
@@ -550,8 +549,8 @@ def ex6_grid_searching_MLP_regs():
     warnings.filterwarnings(action='once')
 
     PATH = "../Datasets/"
-    CSV_DATA = "USA_Housing.csv"
-    df = pd.read_csv(PATH + CSV_DATA)
+    CSV_DATA = "housing.data"
+    df = pd.read_csv(PATH + CSV_DATA, header=None)
 
     # Show all columns.
     pd.set_option('display.max_columns', None)
@@ -566,12 +565,8 @@ def ex6_grid_searching_MLP_regs():
     dataset = df.values
 
     # split into input (X) and output (Y) variables
-    X = df.copy()
-    y = X['Price']
-    del X['Price']
-    del X['Address']
-
-    print(y)
+    X = dataset[:, 0:13]
+    y = dataset[:, 13]
 
     trainX, temp_X, trainY, temp_y = train_test_split(X, y, train_size=0.7)
     valX, testX, valY, testY = train_test_split(temp_X, temp_y, train_size=0.5)
@@ -583,16 +578,16 @@ def ex6_grid_searching_MLP_regs():
     valX_scaled = scalerX.transform(valX)
     testX_scaled = scalerX.transform(testX)
 
-    scY = StandardScaler()
-    trainY_scaled = scY.fit_transform(np.array(trainY).reshape(-1, 1)).ravel()  # Flatten the array
-    testY_scaled = scY.transform(np.array(testY).reshape(-1, 1)).ravel()  # Flatten the array
-    valY_scaled = scY.transform(np.array(valY).reshape(-1, 1)).ravel()  # Flatten the array
+    scY = StandardScaler() #Chat fixed
+    trainY_scaled = scY.fit_transform(np.array(trainY).reshape(-1, 1)).ravel()
+    testY_scaled = scY.transform(np.array(testY).reshape(-1, 1)).ravel()
+    valY_scaled = scY.transform(np.array(valY).reshape(-1, 1)).ravel()
 
     # Build basic multilayer perceptron.
     model1 = MLPRegressor(
         # 3 hidden layers with 150 neurons, 100, and 50.
         hidden_layer_sizes=(150, 100, 50),
-        max_iter=230,  # epochs stops at this point
+        max_iter=50,  # epochs -> increased it. as it could not reach convergence
         activation='relu',
         solver='adam',  # optimizer
         verbose=1)
@@ -604,8 +599,8 @@ def ex6_grid_searching_MLP_regs():
         plt.xlabel('Iterations')
         plt.ylabel('Cost')
         plt.show()
+
     def evaluateModel(model, testX_scaled, testY_scaled, scY):
-        print("Evaluating model")
         showLosses(model)
         scaledPredictions = model.predict(testX_scaled)
         y_pred = scY.inverse_transform(
@@ -613,31 +608,29 @@ def ex6_grid_searching_MLP_regs():
         mse = metrics.mean_squared_error(testY_scaled, y_pred)
         rmse = np.sqrt(mse)
         print("RMSE: " + str(rmse))
-    evaluateModel(model1, valX_scaled, valY_scaled, scY)
+
+    evaluateModel(model1, valX_scaled, valY, scY)
 
     # here is the new part.
     param_grid = {
         'hidden_layer_sizes': [(150, 100, 50), (120, 80, 40), (100, 50, 30)],
-        'max_iter': [200, 250, 300],
+        'max_iter': [50, 100], # changed it to a singular iteration.
         'activation': ['tanh', 'relu'],
         'solver': ['sgd', 'adam'],
         'alpha': [0.0001, 0.05],
         'learning_rate': ['constant', 'adaptive'],
         'learning_rate_init': [0.0001, 0.001, 0.005, 0.01, 0.1, 0.2]
     }
-
-    from sklearn.model_selection import GridSearchCV
+    from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
     # n_jobs=-1 means use all processors.
     # Run print(metrics.get_scorer_names()) for scoring choices.
     model2 = MLPRegressor()
-    gridModel = GridSearchCV(model2, param_grid, n_jobs=-1, cv=10,
-                             scoring='neg_mean_squared_error')
+    gridModel = RandomizedSearchCV(model2, param_grid, n_jobs=-1, cv=10, scoring='neg_mean_squared_error')
     gridModel.fit(trainX_scaled, trainY_scaled)
 
     print("Best parameters")
     print(gridModel.best_params_)
-
     evaluateModel(gridModel.best_estimator_, valX_scaled, valY, scY)
 
     # Evaluate both models with test (unseen) data.
@@ -647,4 +640,4 @@ def ex6_grid_searching_MLP_regs():
     print("\n*** Grid searched model with test data: ")
     evaluateModel(gridModel.best_estimator_, testX_scaled, testY, scY)
     print(gridModel.get_params())
-# ex6_grid_searching_MLP_regs()
+ex6_grid_searching_MLP_regs()
